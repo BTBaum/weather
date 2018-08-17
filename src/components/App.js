06d45482b62687feb5dd2be74f7b63216ed1880f
Brandon Baum <bbaum@redhat.com>
2018-08-17 14:00:33 -0400

    Added axios get call for zip code and return data

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import './App.css'
 import axios from 'axios'
 
+const API_KEY = 'appid=b7c39bcd112707191d11bd37e5c84658'
+const OPEN_WEATHER = 'http://api.openweathermap.org/data/2.5/weather?zip='
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       zip: "",
+      data: null
     }
     this.onInputChange = this.onInputChange.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
@@ -18,6 +22,14 @@ class App extends Component {
 
   onSubmit(event) {
     event.preventDefault()
+    // I would add check to make sure zip has length of 5
+    // before submitting. If not tell user to enter 5 digit zip
+    const zipSearch = `${OPEN_WEATHER}${this.state.zip}&${API_KEY}`
+    axios.get(zipSearch)
+      .then(({ data }) => {
+        console.log("data", data)
+        this.setState({ data })
+      })
   }
 
   render() {
